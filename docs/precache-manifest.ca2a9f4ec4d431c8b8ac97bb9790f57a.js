e189ee3edc7cec5d95d0a05c764ec7ba70419cde
radames vaz <rgvtovar@hotmail.com>
2020-12-03 17:41:16 -0400

    subiendo docs

---
+++ docs/precache-manifest.ca2a9f4ec4d431c8b8ac97bb9790f57a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22d56142f4fddceb79ea426180fa9dfa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bad1955fe05b068eca09",
+    "url": "/static/css/main.9a29dd47.chunk.css"
+  },
+  {
+    "revision": "89cbd70dab58ec3a1804",
+    "url": "/static/js/2.7597bda6.chunk.js"
+  },
+  {
+    "revision": "f5fc2afae58c4ffc30c6148864765d4b",
+    "url": "/static/js/2.7597bda6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bad1955fe05b068eca09",
+    "url": "/static/js/main.cd7b6693.chunk.js"
+  },
+  {
+    "revision": "903e7817fc2a18360c09",
+    "url": "/static/js/runtime-main.5142bbd6.js"
+  }
+]);
